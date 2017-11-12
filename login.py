b03313fd822f1185b6eebeafd334accb22355545
weiguang <chen2621978@gmail.com>
2017-11-11 22:39:39 -0800

    增加了登录失败重试，增加了账号列表，并把最近30天没有登录的账号记录

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 
 import requests
 from recognise import *
@@ -6,6 +6,8 @@ from PIL import Image
 import base64
 import getpass
 import re
+import datetime
+import time
 
 from BeautifulSoup import BeautifulSoup  
 
@@ -13,38 +15,49 @@ from BeautifulSoup import BeautifulSoup
 def login(username,passwd):
     session=requests.session()
     session.get('http://192.168.252.133:8080/selfservice').text
-    img=session.get('http://192.168.252.133:8080/selfservice/common/web/verifycode.jsp').content
-    with open('captcha.jpeg','wb') as imgfile:
-        imgfile.write(img)
-    imageRecognize=CaptchaRecognize()
-    image=Image.open('captcha.jpeg')
-    result=imageRecognize.recognise(image)
-    string=''
-    for item in result:
-        string+=item[1]
-    print(string)
-    data={
-    #'usertype':"xs",
-    'name':username,
-    'password':passwd,
-    'verify':string,
-    'act':"add"
-    #'ln':"app610.dc.hust.edu.cn"
-    }
-    headers = {
-        'Host':"192.168.252.133:8080",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Connection": "keep-alive",
-        'Referer':"http://192.168.252.133:8080/selfservice/module/scgroup/web/login_self.jsf",
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0"}
-    session.post('http://192.168.252.133:8080/selfservice/module/scgroup/web/login_judge.jsf',data=data,headers=headers)
-    html=session.get('http://192.168.252.133:8080/selfservice/module/webcontent/web/index_self.jsf',headers=headers).text
+    while True :
+        img=session.get('http://192.168.252.133:8080/selfservice/common/web/verifycode.jsp').content
+        with open('captcha.jpeg','wb') as imgfile:
+                imgfile.write(img)
+        imageRecognize=CaptchaRecognize()
+        image=Image.open('captcha.jpeg')
+        result=imageRecognize.recognise(image)
+        string=''
+        for item in result:
+                string+=item[1]
+        print(username + ", verify code:" + string)
+        data={
+        #'usertype':"xs",
+        'name':username,
+        'password':passwd,
+        'verify':string,
+        'act':"add"
+        #'ln':"app610.dc.hust.edu.cn"
+        }
+        headers = {
+                'Host':"192.168.252.133:8080",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                "Accept-Encoding": "gzip, deflate",
+                "Accept-Language": "en-US,en;q=0.5",
+                "Connection": "keep-alive",
+                'Referer':"http://192.168.252.133:8080/selfservice/module/scgroup/web/login_self.jsf",
+                "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0"}
+        session.post('http://192.168.252.133:8080/selfservice/module/scgroup/web/login_judge.jsf',data=data,headers=headers)
+        html=session.get('http://192.168.252.133:8080/selfservice/module/webcontent/web/index_self.jsf',headers=headers).text
+        soup = BeautifulSoup(html)
+        flag = html.find(u'您还未登录或会话过期')
+        time.sleep(1)
+        if flag == -1 :
+            break
+        else :
+            print("logn error,retry now!")
     #print(html)
     return session
 
 def onlineDetail(session):
+    toTime = datetime.datetime.now()
+    fromTime = datetime.datetime.now() - datetime.timedelta(days=30)
+
     headers = {
         'Host':"192.168.252.133:8080",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -62,8 +75,8 @@ def onlineDetail(session):
     
     data={
     #'usertype':"xs",
-    'OnlineDetailForm:loginFromTime':'2017-11-5 00:00:00',
-    'OnlineDetailForm:logoutToTime':'2017-11-12 23:59:59',
+    'OnlineDetailForm:loginFromTime':fromTime.strftime("%Y-%m-%d")+' 00:00:00',
+    'OnlineDetailForm:logoutToTime':toTime.strftime("%Y-%m-%d")+' 23:59:59',
     #'OnlineDetailForm:onlineTimeCompare':'onlineDetailSelfListBean.temp',
     'OnlineDetailForm':'OnlineDetailForm',
     'com.sun.faces.VIEW':VIEW,
@@ -76,33 +89,42 @@ def onlineDetail(session):
     html=session.post('http://192.168.252.133:8080/selfservice/module/onlineuserself/web/onlinedetailself_list.jsf',data=data,headers=headers).content
     #print(html)
     soup = BeautifulSoup(html) 
-    tables = soup.findAll('table',{'id':'ec_table'})  
-    tab = tables[0]  
-    for tr in tab.findAll('tr'):  
-        for td in tr.findAll('td'):  
-            print td.getText(),  
-        print 
+    #tables = soup.findAll('table',{'id':'ec_table'})  
+    #tab = tables[0]  
+    #for tr in tab.findAll('tr'):  
+    #    for td in tr.findAll('td'):  
+    #        print td.getText(),  
+    #    print 
 
     table=soup.findAll('table',{'id':'scrollTable'})
     logintimes=table[0].find('td').find('strong').text
-    print(logintimes)
-
-    
-    if logintimes != '当前无记录':
+    #print(logintimes)
+ 
+    if logintimes != u'当前无记录':
         return logintimes
     else:
         return 0 
- 
-   
+
+def check(username,passwd):
+    session = login(username,passwd)
+    logintimes = onlineDetail(session)
+    print "onlinelog:"+str(logintimes)
+    f = open("result.re", 'a+')
+    if(logintimes == 0):
+        print >> f, username 
+
+def checkList(filepath):
+    for line in open(filepath):
+	name=line.strip()
+        check(name,name)
+        print
 
 def main():
     #username=input('username:')
     #passwd=base64.b64encode(getpass.getpass('Passwd:').encode()).decode()
-    username = 'bgnxy111'
-    passwd = username
-    session = login(username,passwd)
-    logintimes = onlineDetail(session)
-    f = open("check.re", 'a+')
-    if(logintimes != 0):
-        print >> f, username
+    #username='bgnxy111'
+    #passwd=username
+    #check(username,passwd)
+    checkList("good_list.re")
+
 main()
